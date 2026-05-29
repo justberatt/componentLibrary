2f6d3ff6a3a03ddd53df8aab41cc061ebb32adad
Berat Sakipi <berat.sakipi@gmail.com>
2026-05-29 21:58:25 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Button from './components/Button/Button'
+
+function App() {
+
+  return (
+    <main>
+      <Button variant='danger'>Log in with Google</Button>
+    </main>
+  )
+}
+
+export default App
